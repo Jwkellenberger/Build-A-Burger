2e3af1de5a2f30beaaa322e09d5cd63141e62b35
Jwkellenberger <32597032+Jwkellenberger@users.noreply.github.com>
2018-06-23 20:35:25 -0400

    added axios interceptor eject on unmount for reusable code!

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,14 +8,21 @@ const withErrorHandler =(WrappedComponent, axios)=>{
             error: null
         }
         
+        //not greatest implementation for re-usability
+        //memory leak, thus, need component will unmount below!
         componentWillMount(){
-            axios.interceptors.request.use(req =>{
+            this.reqInterceptor = axios.interceptors.request.use(req =>{
                 this.setState({error: null});
                 return req;
             })
-            axios.interceptors.response.use(res => res, error =>{
+            this.resInterceptor = axios.interceptors.response.use(res => res, error =>{
                 this.setState({error:error});
             });
+        }
+
+        componentWillUnmount(){
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler=()=>{
